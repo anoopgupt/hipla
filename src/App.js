43786fbcc166555741329d9fc8d75e910b45e409
anoop123gupta <anoop18@navgurukul.org>
2020-06-18 15:05:07 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/signIn';
+import SignUp from './components/Signup';
+import Header from './components/header/headerComponent';
+import SideBar from './components/sidebar/sidebar';
+import AllReports from './components/allReports';
+import CreateNewRequest from './components/createNewRequest';
+import Dashboard from './components/dashboard';
+import Meetings from './components/meetings';
+
+
+import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Router>
+        <Route exact path="/" >
+          <SignIn />  
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+        <Route exact path="/header">
+          <Header />
+        </Route>
+
+        {/* <Route exact path="/dashboard">
+          <Dashboard />
+        </Route> */}
+      </Router>
+        
     </div>
   );
 }
